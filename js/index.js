4697549a46d52a4b336b358cc3e4077ceb24923d
yancleiton7 <74550820+yancleiton7@users.noreply.github.com>
2021-04-21 23:12:52 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,45 @@
+
+
+function capturar(nome, intro, convite,  some, aparece){
+
+    capturando = document.getElementById(nome).value;
+    if (capturando === ""){
+        alert('Eita! Acho que você esqueceu de digitar seu nome! kkkk')
+    }
+    else{
+    
+    muda_pagina(some, aparece);
+    document.getElementById(intro).innerHTML = capturando;
+    document.getElementById(convite).innerHTML = capturando;
+    }
+
+}
+
+
+function show_article(id){
+    
+    var infos = document.getElementById(id);
+   
+    if (infos.style.display === "none") {
+        infos.style.display = "inline-block";
+      } 
+    else {
+        infos.style.display = "none";
+      }
+    
+}
+
+
+function muda_pagina(some, aparece){
+    show_article(some);
+    show_article(aparece);
+    
+    some = '';
+    aparece = '';
+}
+
+function redirect (){
+    window.location.href = "http://www.encurtador.com.br/gxEQ6";
+}
+
+
